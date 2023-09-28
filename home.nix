7eda034103a0991bcd16101749690a96ed77ca11
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-09-28 19:56:11 +0200

    Add rustup

--- home.nix
+++ home.nix
@@ -15,6 +15,7 @@
     ./programs/kitty.nix
     ./programs/neovim/neovim.nix
     ./programs/ripgrep.nix
+    ./programs/rust.nix
     ./programs/tmux/tmux.nix
     ./programs/utils.nix
     ./programs/zsh.nix
