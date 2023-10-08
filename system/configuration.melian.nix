3efdce658f8e07eac85e0a6f7077b3f3cbc54e93
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-10-08 17:21:21 +0200

    Disable power-profiles-daemon for melian

--- system/configuration.melian.nix
+++ system/configuration.melian.nix
@@ -2,7 +2,9 @@
   imports = [./configuration.nix ./hardware-configuration.melian.nix];
 
   networking.hostName = "melian";
-  services.tlp.enable = true;
   boot.loader.timeout = 0;
   boot.loader.systemd-boot.enable = true;
+
+  services.tlp.enable = true;
+  services.power-profiles-daemon.enable = false;
 }
