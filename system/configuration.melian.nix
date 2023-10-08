7a30183c66b16a78e63bbc022db521f5c9182618
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-10-08 17:15:52 +0200

    Enable tlp for melian

--- system/configuration.melian.nix
+++ system/configuration.melian.nix
@@ -2,6 +2,7 @@
   imports = [./configuration.nix ./hardware-configuration.melian.nix];
 
   networking.hostName = "melian";
+  services.tlp.enable = true;
   boot.loader.timeout = 0;
   boot.loader.systemd-boot.enable = true;
 }
