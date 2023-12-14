542a11d7bfbb522317caa32eed1ff115488eb78e
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-12-14 01:10:16 +0100

    Add nvim-lsp-file-operations

--- user/programs/neovim/config/lua/plugins/tree.lua
+++ user/programs/neovim/config/lua/plugins/tree.lua
@@ -1,17 +1,30 @@
 local constants = require("constants")
 return {
-  "nvim-tree/nvim-tree.lua",
-  lazy = true,
-  cmd = { "NvimTreeToggle", "NvimTreeFindFile" },
-  opts = {
-    view = { width = constants.file_explorer_width_chars, },
-    diagnostics = { enable = false },
-    renderer = { group_empty = true },
-  },
-  init = function()
-    local m = require("mapx")
+  {
+    "nvim-tree/nvim-tree.lua",
+    lazy = true,
+    cmd = { "NvimTreeToggle", "NvimTreeFindFile" },
+    opts = {
+      view = { width = constants.file_explorer_width_chars },
+      diagnostics = { enable = false },
+      renderer = { group_empty = true },
+    },
+    init = function()
+      local m = require("mapx")
 
-    m.nmap("<leader>e", ":NvimTreeToggle<cr>", { silent = true }, "Toggle the file explorer view")
-    m.nmap("<leader>E", ":NvimTreeFindFile<cr>", { silent = true }, "Find the current file in the explorer")
-  end
+      m.nmap("<leader>e", ":NvimTreeToggle<cr>", { silent = true }, "Toggle the file explorer view")
+      m.nmap("<leader>E", ":NvimTreeFindFile<cr>", { silent = true }, "Find the current file in the explorer")
+    end,
+  },
+  {
+    "antosha417/nvim-lsp-file-operations",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-tree.lua",
+    },
+    enable = false,
+    config = function()
+      require("lsp-file-operations").setup()
+    end,
+  },
 }
