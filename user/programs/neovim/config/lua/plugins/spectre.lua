077e373986daee75039c682e619ba209dbacdeb3
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-02-18 00:32:43 +0100

    Fix spectre so that it doesn't break devicons

--- user/programs/neovim/config/lua/plugins/spectre.lua
+++ user/programs/neovim/config/lua/plugins/spectre.lua
@@ -10,24 +10,23 @@ return {
   },
   init = function()
     local m = require("mapx")
-    local spectre = require("spectre")
 
     m.nname("<leader>s", "Search")
     m.nmap("<leader>sr", function()
-      spectre.toggle()
+      require("spectre").toggle()
     end, "Search and replace")
 
     m.nmap("<leader>sw", function()
-      spectre.open_visual({ select_word = true })
+      require("spectre").open_visual({ select_word = true })
     end, "Search and replace current word")
 
     m.xmap("<leader>sw", function()
       vim.cmd("esc")
-      spectre.open_visual()
+      require("spectre").open_visual()
     end, "Search and replace current word")
 
     m.nmap("<leader>sp", function()
-      spectre.open_file_search({ select_word = true })
+      require("spectre").open_file_search({ select_word = true })
     end, "Search and replace on current file")
   end,
 }
