3e4a94ee39ed50f074c360dd3246d6314dd47826
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-02-18 10:38:44 +0100

    Call setup on nvim-highlight-colors

--- user/programs/neovim/config/lua/plugins/init.lua
+++ user/programs/neovim/config/lua/plugins/init.lua
@@ -6,8 +6,8 @@ return {
   { "folke/neodev.nvim", opts = { library = { plugins = { "neotest" }, types = true } }, priority = 51 },
   { "nvim-lua/plenary.nvim", priority = 51 },
   -- LSP & UI
-  { "brenoprata10/nvim-highlight-colors" },
-  { "folke/todo-comments.nvim", opts = {} },
+  { "brenoprata10/nvim-highlight-colors", config = true },
+  { "folke/todo-comments.nvim", config = true },
   { "folke/trouble.nvim" },
   { "mbbill/undotree" },
   { "nvim-tree/nvim-web-devicons" },
