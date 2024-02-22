c3c8796fd0bcf09a26db2371e3a407b7373eb904
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-02-22 17:47:05 +0100

    Set nvim-tree borders

--- user/programs/neovim/config/lua/plugins/tree.lua
+++ user/programs/neovim/config/lua/plugins/tree.lua
@@ -5,10 +5,14 @@ return {
     lazy = true,
     cmd = { "NvimTreeToggle", "NvimTreeFindFile" },
     opts = {
-      view = { width = constants.file_explorer_width_chars },
+      view = {
+        width = constants.file_explorer_width_chars,
+        float = { open_win_config = { border = constants.floating_border } },
+      },
       diagnostics = { enable = false },
       renderer = { group_empty = true },
       disable_netrw = true,
+      actions = { file_popup = { open_win_config = { border = constants.floating_border } } },
     },
     init = function()
       local m = require("mapx")
