dacab0411d2bb48af54fbb147a58ca3dce7ded34
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-02-24 02:03:34 +0100

    Fix/complete system volta installation

--- user/programs/javascript.nix
+++ user/programs/javascript.nix
@@ -1,3 +1,11 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: let
+  voltaHome = "${config.home.homeDirectory}/.volta";
+in {
   home.packages = with pkgs; [volta];
+  home.sessionVariables.VOLTA_HOME = voltaHome;
+  home.sessionPath = ["${voltaHome}/bin"];
 }
