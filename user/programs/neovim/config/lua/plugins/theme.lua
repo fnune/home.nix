07baae14bce2d289307245c2261d8824bd5fc5c8
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-02-25 11:41:08 +0100

    Use upstreamed vscode.nvim changes

--- user/programs/neovim/config/lua/plugins/theme.lua
+++ user/programs/neovim/config/lua/plugins/theme.lua
@@ -1,46 +1,3 @@
-function LinkNvimDapHighlights()
-  vim.cmd([[
-    hi! link DapUIPlayPauseNC DapUIPlayPause
-    hi! link DapUIRestartNC DapUIRestart
-    hi! link DapUIStepBackNC DapUIStepBack
-    hi! link DapUIStepIntoNC DapUIStepInto
-    hi! link DapUIStepOutNC DapUIStepOut
-    hi! link DapUIStepOverNC DapUIStepOver
-    hi! link DapUIStopNC DapUIStop
-
-    hi! link DapUIPlayPause DiagnosticOk
-    hi! link DapUIRestart DiagnosticOk
-    hi! link DapUIStepBack Macro
-    hi! link DapUIStepInto Macro
-    hi! link DapUIStepOut Macro
-    hi! link DapUIStepOver Macro
-    hi! link DapUIStop DiagnosticError
-    hi! link DapUIType Macro
-    hi! link DapUIWinSelect DiagnosticHint
-  ]])
-end
-
-function LinkNeotestHighlights()
-  vim.cmd([[
-    hi! link NeotestAdapterName Macro
-    hi! link NeotestDir Normal
-    hi! link NeotestExpandMarker StatusLine
-    hi! link NeotestFailed DiagnosticError
-    hi! link NeotestFile Normal
-    hi! link NeotestFocused Underlined
-    hi! link NeotestIndent StatusLine
-    hi! link NeotestMarked DiagnosticHint
-    hi! link NeotestNamespace Macro
-    hi! link NeotestPassed DiagnosticOk
-    hi! link NeotestRunning DiagnosticWarn
-    hi! link NeotestSkipped StatusLine
-    hi! link NeotestTarget Macro
-    hi! link NeotestTest Boolean
-    hi! link NeotestWinSelect Macro
-    hi! link NeotestUnknown StatusLine
-  ]])
-end
-
 function HideEndOfBufferCharacters()
   vim.cmd([[hi EndOfBuffer guifg=bg guibg=bg]])
 end
@@ -82,8 +39,6 @@ end
 
 function ApplyCommonHighlights()
   vim.opt.termguicolors = true
-  LinkNvimDapHighlights()
-  LinkNeotestHighlights()
   HighlightItalics()
   HideEndOfBufferCharacters()
 end
@@ -103,18 +58,7 @@ return {
         underline_links = true,
         group_overrides = {
           ["@comment"] = { fg = colors.vscGray },
-          ["BufferTabpageFill"] = { bg = colors.vscTabOther },
           ["Comment"] = { fg = colors.vscGray },
-          ["NeogitDiffAdd"] = { fg = colors.vscGitAdded, bg = colors.vscDiffGreenDark },
-          ["NeogitDiffAddHighlight"] = { fg = colors.vscGitAdded, bg = colors.vscDiffGreenLight },
-          ["NeogitDiffContext"] = { fg = colors.vscPopupFront, bg = colors.vscLeftDark },
-          ["NeogitDiffContextHighlight"] = { fg = colors.vscPopupFront, bg = colors.vscLeftMid },
-          ["NeogitDiffDelete"] = { fg = colors.vscGitDeleted, bg = colors.vscDiffRedDark },
-          ["NeogitDiffDeleteHighlight"] = { fg = colors.vscGitDeleted, bg = colors.vscDiffRedLight },
-          ["NeogitDiffHeader"] = { fg = colors.vscSplitLight, bg = colors.vscBack },
-          ["NeogitDiffHeaderHighlight"] = { fg = colors.vscSplitLight, bg = colors.vscBack },
-          ["NeogitHunkHeader"] = { fg = colors.vscSplitLight, bg = colors.vscDiffGreenDark },
-          ["NeogitHunkHeaderHighlight"] = { fg = colors.vscSplitLight, bg = colors.vscDiffGreenDark },
           ["Pmenu"] = { fg = colors.vscPopupFront, bg = "NONE" },
           ["PmenuSel"] = { fg = "NONE", bg = colors.vscPopupHighlightBlue },
           ["SpecialComment"] = { fg = colors.vscGray },
