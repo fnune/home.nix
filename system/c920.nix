7cc60508095fee0c66e922a94da57eb2af6ed414
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-03-06 11:29:55 +0100

    Adjust focus after having moved my webcam

--- system/c920.nix
+++ system/c920.nix
@@ -18,14 +18,14 @@
           DEVICE=$(${v4l2Ctl} --list-devices | grep -A1 C920 | tail -n1 | xargs)
           if [ $DEVICE ]; then
             echo "Configuring C920 camera settings..."
-            ${v4l2Ctl} -d $DEVICE --set-ctrl=focus_automatic_continuous=0 2> /dev/null || ${v4l2Ctl} -d $DEVICE --set-ctrl=focus_auto=0
+            ${v4l2Ctl} -d $DEVICE --set-ctrl=auto_exposure=3
+            ${v4l2Ctl} -d $DEVICE --set-ctrl=focus_absolute=130
+            ${v4l2Ctl} -d $DEVICE --set-ctrl=focus_automatic_continuous=0
             ${v4l2Ctl} -d $DEVICE --set-ctrl=gain=190
-            ${v4l2Ctl} -d $DEVICE --set-ctrl=zoom_absolute=125
-            ${v4l2Ctl} -d $DEVICE --set-ctrl=focus_absolute=100
             ${v4l2Ctl} -d $DEVICE --set-ctrl=pan_absolute=1400
-            ${v4l2Ctl} -d $DEVICE --set-ctrl=white_balance_automatic=1 2> /dev/null || ${v4l2Ctl} -d $DEVICE --set-ctrl=white_balance_temperature_auto=1
             ${v4l2Ctl} -d $DEVICE --set-ctrl=sharpness=160
-            ${v4l2Ctl} -d $DEVICE --set-ctrl=auto_exposure=3 2> /dev/null || ${v4l2Ctl} -d $DEVICE --set-ctrl=exposure_auto=3
+            ${v4l2Ctl} -d $DEVICE --set-ctrl=white_balance_automatic=1
+            ${v4l2Ctl} -d $DEVICE --set-ctrl=zoom_absolute=125
             echo "C920 camera setup completed."
           else
             echo "No C920 camera found."
