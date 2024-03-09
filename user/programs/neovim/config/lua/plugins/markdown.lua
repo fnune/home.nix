4c5edc335d876b0d45745f7efebc36ce98579ee5
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-03-09 21:03:16 +0100

    Allow prettier & vim-markdown-toc autogen to coexist

--- user/programs/neovim/config/lua/plugins/markdown.lua
+++ user/programs/neovim/config/lua/plugins/markdown.lua
@@ -1,4 +1,10 @@
 return {
   { "jxnblk/vim-mdx-js" },
-  { "mzlogin/vim-markdown-toc" },
+  {
+    "mzlogin/vim-markdown-toc",
+    init = function()
+      vim.g.vmt_auto_update_on_save = true
+      vim.g.vmt_list_item_char = "-"
+    end,
+  },
 }
