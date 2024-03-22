681c28521c09073953e808f50560a7d8ba1478f9
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-03-22 12:54:20 +0100

    Enable difftastic for git

--- user/programs/git.nix
+++ user/programs/git.nix
@@ -1,10 +1,14 @@
-{config, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
+  home.packages = [pkgs.unstable.difftastic];
   programs.gpg.enable = true;
   programs.git = {
     enable = true;
     userName = "Fausto Núñez Alberro";
     userEmail = "fausto.nunez@mailbox.org";
-    delta.enable = true;
     includes = [
       {
         path = "${config.home.homeDirectory}/.local/gitconfig";
@@ -27,6 +31,12 @@
       pull.ff = "only";
       push.autoSetupRemote = true;
       push.default = "current";
+      pager.difftool = true;
+      diff.tool = "difftastic";
+      difftool = {
+        prompt = false;
+        difftastic.cmd = "${pkgs.unstable.difftastic}/bin/difft \"$LOCAL\" \"$REMOTE\"";
+      };
     };
   };
 }
