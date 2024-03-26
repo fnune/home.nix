1108a488379fc01345740ec2f31a990e4b8366e9
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-03-26 12:49:35 +0100

    Use <leader>hb for git blame instead of <leader>Db

--- user/programs/neovim/config/lua/plugins/fugitive.lua
+++ user/programs/neovim/config/lua/plugins/fugitive.lua
@@ -6,6 +6,6 @@ return {
     vim.cmd([[let $GIT_CONFIG_PARAMETERS = "'blame.date=short'"]])
 
     local m = require("mapx")
-    m.nmap("<leader>Db", ":Git blame<cr>", { silent = true }, "Open git blame view")
+    m.nmap("<leader>hb", ":Git blame<cr>", { silent = true }, "Open git blame view")
   end,
 }
