692895061818e225a496006dd0530f7e6a611291
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-06-26 11:14:45 +0200

    Show LSP progress in Neovim

--- user/programs/neovim/config/lua/plugins/lualine.lua
+++ user/programs/neovim/config/lua/plugins/lualine.lua
@@ -1,8 +1,13 @@
 return {
   "nvim-lualine/lualine.nvim",
+  dependencies = { "linrongbin16/lsp-progress.nvim" },
   config = function()
     local lualine = require("lualine")
     local timetracking = require("timetracking")
+
+    local lsp_progress = require("lsp-progress")
+    lsp_progress.setup({})
+
     lualine.setup({
       options = {
         component_separators = { "│", "│" },
@@ -14,7 +19,12 @@ return {
       sections = {
         lualine_a = { "mode" },
         lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = { { "filename", path = 1 } },
+        lualine_c = {
+          { "filename", path = 1 },
+          function()
+            return require("lsp-progress").progress()
+          end,
+        },
         lualine_x = { timetracking.WatsonStatus, "filetype" },
         lualine_y = { "progress" },
         lualine_z = { "location" },
