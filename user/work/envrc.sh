2ac566b7add7280026e0e5104bc0d31f75346472
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-07-04 15:54:10 +0200

    Reduce worker concurrency to 3 from 10

--- user/work/envrc.sh
+++ user/work/envrc.sh
@@ -4,7 +4,7 @@ if [ -z "$IN_NIX_SHELL" ]; then
   use flake ~/.home.nix/user/work/nix/miniforge
 fi
 
-export CELERY_WORKER_CONCURRENCY=10
+export CELERY_WORKER_CONCURRENCY=3
 export INVOKE_MEMFAULT_PROCFILE_RUNNER=overmind
 export PYRIGHT_DAEMON_ENABLED=1
 export VOLTA_HOME="$HOME/.mambaforge/envs/memfault/.volta"
