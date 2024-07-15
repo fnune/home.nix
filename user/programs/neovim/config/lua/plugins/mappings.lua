6eae1dd0403b22201101f12542c72457232167c9
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-07-15 10:19:00 +0200

    Use which-key v2 because of incompatibility with mapx

--- user/programs/neovim/config/lua/plugins/mappings.lua
+++ user/programs/neovim/config/lua/plugins/mappings.lua
@@ -7,6 +7,7 @@ return {
   },
   {
     "folke/which-key.nvim",
+    tag = "v2.1.0", -- v3 schema is incompatible with mapx
     opts = {
       win = { border = constants.floating_border, padding = { 0, 0, 0, 0 } },
     },
