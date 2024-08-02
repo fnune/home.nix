007cbd7482a8b3185c49e2425a791d6bd902ed89
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-08-02 18:11:33 +0200

    Try persisted.nvim instead of neovim-session-manager

--- user/programs/neovim/config/lua/plugins/session-manager.lua
+++ user/programs/neovim/config/lua/plugins/session-manager.lua
@@ -1,12 +1,5 @@
 return {
-  "Shatur/neovim-session-manager",
-  config = function()
-    local config = require("session_manager.config")
-    local session_manager = require("session_manager")
-
-    session_manager.setup({
-      autoload_mode = config.AutoloadMode.CurrentDir,
-      autosave_ignore_not_normal = false, -- Save the session even if it's empty
-    })
-  end,
+  "olimorris/persisted.nvim",
+  lazy = false,
+  opts = { autosave = true, autoload = true },
 }
