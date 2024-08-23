11ba1680a48d134989b8dc564e392ca2381058c9
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-08-23 15:47:56 +0200

    Add logseq

--- user/desktop/applications.nix
+++ user/desktop/applications.nix
@@ -1,10 +1,12 @@
 {pkgs, ...}: {
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.allowUnfreePredicate = _: true;
+  nixpkgs.config.permittedInsecurePackages = ["electron-27.3.11"]; # For logseq
 
   home.packages = with pkgs.unstable; [
     kdenlive
     libreoffice
+    logseq
     lutris
     obsidian
     quickemu
