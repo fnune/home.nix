19ae95b6d24986c6935cf02fa8d15118fd546157
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-09-01 09:31:12 +0200

    Remove unclutter, does not work with Wayland and breaks LSP VSTs

--- system/configuration.nix
+++ system/configuration.nix
@@ -29,7 +29,6 @@
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  services.unclutter.enable = true;
   services.geoclue2.enable = true;
   services.flatpak.enable = true;
 
