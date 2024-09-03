7663ca16b598386750829f43dc0ede4a1384e726
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-09-03 23:06:13 +0200

    Remove protonmail-bridge

--- user/programs/thunderbird.nix
+++ user/programs/thunderbird.nix
@@ -1,6 +1,4 @@
 {pkgs, ...}: {
-  home.packages = [pkgs.protonmail-bridge];
-
   programs = {
     thunderbird = {
       enable = true;
@@ -14,26 +12,6 @@
         "mailnews.default_sort_type" = 18;
         "mailnews.message_display.disable_remote_image" = false;
         "mailnews.start_page.enabled" = false;
-      };
-    };
-  };
-
-  systemd.user.services = {
-    "protonmail-bridge" = {
-      Unit = {
-        Description = "ProtonMail Bridge";
-        PartOf = ["graphical-session.target"];
-        After = ["network.target"];
-      };
-      Install = {
-        WantedBy = ["default.target"];
-      };
-      Service = {
-        ExecStart = "${pkgs.protonmail-bridge}/bin/protonmail-bridge --noninteractive";
-        Restart = "always";
-        Environment = "PATH=${pkgs.gnome3.gnome-keyring}/bin";
-        StandardOutput = "journal";
-        StandardError = "journal";
       };
     };
   };
