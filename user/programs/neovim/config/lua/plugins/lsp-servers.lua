7fa914002742320d4d279c6be740e53af04c1e23
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-09-05 17:25:58 +0200

    Try out typescript-tools.nvim instead of ts_ls

--- user/programs/neovim/config/lua/plugins/lsp-servers.lua
+++ user/programs/neovim/config/lua/plugins/lsp-servers.lua
@@ -1,73 +1,56 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = { "b0o/SchemaStore.nvim", "hrsh7th/cmp-nvim-lsp" },
-  config = function()
-    local lspconfig = require("lspconfig")
-    local cmp_lsp = require("cmp_nvim_lsp")
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {},
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { "b0o/SchemaStore.nvim", "hrsh7th/cmp-nvim-lsp" },
+    config = function()
+      local lspconfig = require("lspconfig")
+      local cmp_lsp = require("cmp_nvim_lsp")
 
-    local function setup(lsp, opts)
-      lspconfig[lsp].setup(vim.tbl_deep_extend("force", {
-        capabilities = cmp_lsp.default_capabilities(),
-      }, opts))
-    end
+      local function setup(lsp, opts)
+        lspconfig[lsp].setup(vim.tbl_deep_extend("force", {
+          capabilities = cmp_lsp.default_capabilities(),
+        }, opts))
+      end
 
-    setup("biome", {})
-    setup("clangd", {})
-    setup("eslint", {})
-    setup("gdscript", {})
-    setup("ruff_lsp", {})
-    setup("rust_analyzer", {})
-    setup("tailwindcss", {})
-    setup("taplo", {})
+      setup("biome", {})
+      setup("clangd", {})
+      setup("eslint", {})
+      setup("gdscript", {})
+      setup("ruff_lsp", {})
+      setup("rust_analyzer", {})
+      setup("tailwindcss", {})
+      setup("taplo", {})
 
-    setup("nil_ls", {
-      init_options = { nix = { flake = { autoArchive = true } } },
-    })
+      setup("nil_ls", {
+        init_options = { nix = { flake = { autoArchive = true } } },
+      })
 
-    setup("ts_ls", {
-      init_options = {
-        hostInfo = "neovim",
-        preferences = { importModuleSpecifierPreference = "non-relative" },
-      },
-      handlers = {
-        ["textDocument/formatting"] = function()
-          -- noop: let prettier do this
+      setup("jsonls", {
+        settings = { json = { schemas = require("schemastore").json.schemas(), validate = { enable = true } } },
+      })
+
+      setup("yamlls", {
+        settings = { yaml = { schemas = require("schemastore").yaml.schemas() } },
+      })
+
+      setup("lua_ls", {
+        settings = { Lua = { diagnostics = { globals = { "vim" } } } },
+      })
+
+      setup("pyright", {
+        root_dir = function()
+          return vim.fn.getcwd()
         end,
-      },
-      on_attach = function(client, _)
-        if lspconfig.util.root_pattern("deno.json", "deno.jsonc")(vim.fn.getcwd()) then
-          client.stop()
-        end
-      end,
-    })
+      })
 
-    setup("jsonls", {
-      settings = { json = { schemas = require("schemastore").json.schemas(), validate = { enable = true } } },
-    })
-
-    setup("yamlls", {
-      settings = { yaml = { schemas = require("schemastore").yaml.schemas() } },
-    })
-
-    setup("lua_ls", {
-      settings = { Lua = { diagnostics = { globals = { "vim" } } } },
-    })
-
-    setup("pyright", {
-      root_dir = function()
-        return vim.fn.getcwd()
-      end,
-    })
-
-    setup("denols", {
-      root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
-      on_attach = function(_, _)
-        for _, client in ipairs(vim.lsp.get_active_clients()) do
-          if client.name == "tsserver" then
-            client.stop()
-          end
-        end
-      end,
-    })
-  end,
+      setup("denols", {
+        root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
+      })
+    end,
+  },
 }
