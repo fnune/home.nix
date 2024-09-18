32d0cc9a748650b36c4aa20c975d8fd438982641
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-09-19 01:43:17 +0200

    Try Haruna instead of VLC

--- user/desktop/applications.nix
+++ user/desktop/applications.nix
@@ -4,13 +4,13 @@
   nixpkgs.config.permittedInsecurePackages = ["electron-27.3.11"]; # For logseq
 
   home.packages = with pkgs; [
+    haruna
     logseq
     lutris
     obsidian
     screenkey
     signal-desktop
     spotify
-    vlc
     yubioath-flutter
   ];
 }
