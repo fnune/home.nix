bdf37b920a7d5605bb3e93692fb9a2b39da73299
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-09-19 17:04:20 +0200

    Fix Playwright version linking

--- home/programs/playwright.nix
+++ home/programs/playwright.nix
@@ -6,7 +6,7 @@
 }: let
   dstBrowsersPath = "${config.home.homeDirectory}/.local/share/playwright";
   srcBrowsersPath = pkgs.unstable.playwright-driver.browsers;
-  dstChromiumVersions = []; # https://github.com/microsoft/playwright/blob/v1.41.2/packages/playwright-core/browsers.json
+  dstChromiumVersions = ["1134"]; # https://github.com/microsoft/playwright/blob/v1.47.1/packages/playwright-core/browsers.json
 in {
   home.activation.createChromiumSymlinks = lib.hm.dag.entryAfter ["writeBoundary"] ''
     #!${pkgs.runtimeShell}
@@ -16,11 +16,7 @@ in {
     srcVersion=$(ls ${srcBrowsersPath} | grep chromium- | sed 's/chromium-//')
 
     for version in ${lib.concatStringsSep " " dstChromiumVersions}; do
-      if [[ "$version" == "$srcVersion" ]]; then
-        echo -e "\033[33mPlaywright: chromium-$version is the source version. Consider removing it from 'dstChromiumVersions'."
-      else
-        ln -sf ${srcBrowsersPath}/chromium-$srcVersion ${dstBrowsersPath}/chromium-$version
-      fi
+      ln -sf ${srcBrowsersPath}/chromium-$srcVersion ${dstBrowsersPath}/chromium-$version
     done
   '';
   home.sessionVariables = {
