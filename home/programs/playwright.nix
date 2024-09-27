5e46a4b35eae391546147d70f889438b931f2fe7
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-09-27 14:17:58 +0200

    Improve how I grab the right Playwright

--- home/programs/playwright.nix
+++ home/programs/playwright.nix
@@ -1,26 +1,12 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}: let
-  dstBrowsersPath = "${config.home.homeDirectory}/.local/share/playwright";
-  srcBrowsersPath = pkgs.playwright-driver.browsers;
-  dstChromiumVersions = ["1134"]; # https://github.com/microsoft/playwright/blob/v1.47.1/packages/playwright-core/browsers.json
+{pkgs, ...}: let
+  playwright-browsers = pkgs.callPackage ./playwright-browsers.nix {
+    version = "1.47.1";
+    sha256 = "sha256-tPlzDBxDunUrQFqJZTc6FS+pUkww5x9IpWtCvNIS8Mg";
+  };
 in {
-  home.activation.createChromiumSymlinks = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    #!${pkgs.runtimeShell}
-    rm -rf ${dstBrowsersPath}
-    mkdir -p ${dstBrowsersPath}
-
-    srcVersion=$(ls ${srcBrowsersPath} | grep chromium- | sed 's/chromium-//')
-
-    for version in ${lib.concatStringsSep " " dstChromiumVersions}; do
-      ln -sf ${srcBrowsersPath}/chromium-$srcVersion ${dstBrowsersPath}/chromium-$version
-    done
-  '';
+  home.packages = [playwright-browsers];
   home.sessionVariables = {
-    PLAYWRIGHT_BROWSERS_PATH = dstBrowsersPath;
+    PLAYWRIGHT_BROWSERS_PATH = "${playwright-browsers}";
     PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = "true";
   };
 }
