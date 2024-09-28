838768d6e7644ab0f6ef2a88e77f612d8d67e34a
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-09-29 00:01:27 +0200

    Add keybindings for oil.nvim

--- home/programs/neovim/config/lua/plugins/oil.lua
+++ home/programs/neovim/config/lua/plugins/oil.lua
@@ -2,4 +2,21 @@ return {
   "stevearc/oil.nvim",
   opts = {},
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  keys = {
+    {
+      "<leader>o",
+      function()
+        require("oil").open()
+      end,
+      desc = "Toggle the directory explorer view",
+    },
+    {
+      "<leader>o",
+      function()
+        require("oil").close()
+      end,
+      ft = "oil",
+      desc = "Toggle the directory explorer view",
+    },
+  },
 }
