a21edb8cd55b4cee69fd2cb466cecfb061433b8f
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-10-01 12:49:18 +0200

    Remove unnecessary neotest-python bin hinting

--- home/work/nvim.lua
+++ home/work/nvim.lua
@@ -1,15 +1,9 @@
-local function mamba(path)
-  return os.getenv("MAMBA_ROOT_PREFIX") .. path
-end
-
-local memfault_python_bin = mamba("/envs/memfault/bin/python")
-
 local neotest = require("neotest")
 
 neotest.setup_project(vim.loop.cwd(), {
   adapters = {
     require("neotest-jest")({ jestCommand = "yarn workspace @memfault/app-frontend test:jest" }),
-    require("neotest-python")({ python = memfault_python_bin, args = { "-W", "ignore", "-vv" } }),
+    require("neotest-python")({ args = { "-W", "ignore", "-vv" } }),
     require("neotest-vitest")({ vitestCommand = "yarn vitest" }),
   },
 })
