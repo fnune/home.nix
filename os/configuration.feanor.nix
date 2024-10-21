95db9cf7da358269f921580ad304f7b8d86e7b35
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-10-21 23:41:23 +0200

    Fix some lint errors

--- os/configuration.feanor.nix
+++ os/configuration.feanor.nix
@@ -5,17 +5,21 @@
 
   # Dual boot
   environment.systemPackages = [pkgs.os-prober];
-  boot.loader.timeout = null; # Infinite time to choose
-  boot.loader.grub = {
-    enable = true;
-    device = "nodev";
-    efiInstallAsRemovable = true;
-    efiSupport = true;
-    useOSProber = true;
-    gfxmodeEfi = "2560x1440";
-    gfxpayloadEfi = "2560x1440x32";
+  boot = {
+    plymouth.extraConfig = "DeviceScale=1";
+    loader = {
+      timeout = null; # Infinite time to choose
+      grub = {
+        enable = true;
+        device = "nodev";
+        efiInstallAsRemovable = true;
+        efiSupport = true;
+        useOSProber = true;
+        gfxmodeEfi = "2560x1440";
+        gfxpayloadEfi = "2560x1440x32";
+      };
+    };
   };
-  boot.plymouth.extraConfig = "DeviceScale=1";
   time.hardwareClockInLocalTime = true; # Windows gets confused otherwise
 
   swapDevices = [
