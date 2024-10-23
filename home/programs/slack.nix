79961901acb35a9d12299fd2979a5f5408e57615
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-10-23 09:56:07 +0200

    Remove Slack hacks to enable 'WebRTCPipeWireCapturer'
    It seems like >= 4.38 has it by default now:
    https://forums.slackcommunity.com/s/question/0D53a00009BSEGACA5/when-will-slack-support-wayland-screen-sharing-does-anyone-have-workarounds-or-hacks-to-make-it-work?language=en_US

--- home/programs/slack.nix
+++ home/programs/slack.nix
@@ -1,16 +1,3 @@
 {pkgs, ...}: {
-  nixpkgs.overlays = [
-    (final: prev: {
-      # Fix slack screen sharing following:
-      # https://github.com/flathub/com.slack.Slack/issues/101#issuecomment-1807073763
-      slack = prev.slack.overrideAttrs (previousAttrs: {
-        installPhase =
-          previousAttrs.installPhase
-          + ''
-            sed -i'.backup' -e 's/,"WebRTCPipeWireCapturer"/,"LebRTCPipeWireCapturer"/' $out/lib/slack/resources/app.asar
-          '';
-      });
-    })
-  ];
   home.packages = with pkgs; [slack];
 }
