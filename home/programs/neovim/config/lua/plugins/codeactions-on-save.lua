3a7ed3d576f9f5a6a955946fafda730d1b7d7778
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-10-26 02:54:46 +0200

    Use newer codeactions-on-save and enable for Python

--- home/programs/neovim/config/lua/plugins/codeactions-on-save.lua
+++ home/programs/neovim/config/lua/plugins/codeactions-on-save.lua
@@ -2,6 +2,7 @@ return {
   "fnune/codeactions-on-save.nvim",
   config = function()
     local cos = require("codeactions-on-save")
+    cos.register({ "*.py" }, "source.organizeImports")
     cos.register({ "*.ts", "*.tsx" }, "source.organizeImports.biome")
   end,
 }
