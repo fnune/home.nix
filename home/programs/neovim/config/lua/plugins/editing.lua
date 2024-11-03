4d22f465a0bfd6df6a65270e63a0ddbf84021b6a
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-11-03 10:19:56 +0100

    Add stickybuf.nvim

--- home/programs/neovim/config/lua/plugins/editing.lua
+++ home/programs/neovim/config/lua/plugins/editing.lua
@@ -1,9 +1,10 @@
 return {
-  { "backdround/improved-ft.nvim", opts = { use_default_mappings = true } },
   { "LunarVim/bigfile.nvim" },
+  { "backdround/improved-ft.nvim", opts = { use_default_mappings = true } },
   { "christoomey/vim-tmux-navigator" },
   { "machakann/vim-swap" },
   { "matze/vim-move" },
+  { "stevearc/stickybuf.nvim", config = true },
   { "tpope/vim-eunuch" },
   { "tpope/vim-repeat" },
   { "tpope/vim-surround" },
