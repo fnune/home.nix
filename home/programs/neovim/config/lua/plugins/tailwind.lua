b152df074a74d8bf2dd32f32818affe7e77de293
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-11-03 11:59:21 +0100

    Sort tailwind classes on save of tsx

--- home/programs/neovim/config/lua/plugins/tailwind.lua
+++ home/programs/neovim/config/lua/plugins/tailwind.lua
@@ -7,8 +7,25 @@ return {
     "nvim-telescope/telescope.nvim",
     "neovim/nvim-lspconfig",
   },
-  opts = {
-    document_color = { enabled = false },
-    conceal = { enabled = false },
-  },
+  config = function()
+    local tailwind = require("tailwind-tools")
+
+    tailwind.setup({
+      document_color = { enabled = false },
+      conceal = { enabled = false },
+    })
+
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(args)
+        if vim.lsp.get_client_by_id(args.data.client_id).name == "tailwindcss" then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = args.buf,
+            callback = function()
+              vim.cmd("TailwindSortSync")
+            end,
+          })
+        end
+      end,
+    })
+  end,
 }
