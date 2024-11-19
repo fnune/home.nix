8d31ebae94c183eee7a1bf2a343756fdfd2106bd
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-11-19 08:11:52 +0100

    Upgrade to NixOS 24.11

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "fnune's NixOS configuration files";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixpkgs-development.url = "github:fnune/nixpkgs/fnune/testing";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     plasma-manager = {
