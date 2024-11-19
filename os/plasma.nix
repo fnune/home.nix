815865ea7336b38df30482e5958dce6b1cefe12d
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-11-19 08:19:40 +0100

    Use stable version of koi

--- os/plasma.nix
+++ os/plasma.nix
@@ -24,7 +24,7 @@ in {
   programs.kdeconnect.enable = true;
 
   environment.systemPackages = [
-    pkgs.development.kdePackages.koi
+    pkgs.kdePackages.koi
     pkgs.simp1e-cursors
     wallpaper-sddm
   ];
