08b3cca024e27d1893ada58819eee05fcbe87008
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-11-21 18:50:33 +0100

    Let obs use global shortcuts

--- home/programs/obs-studio.nix
+++ home/programs/obs-studio.nix
@@ -1,3 +1,19 @@
 {pkgs, ...}: {
-  home.packages = [pkgs.obs-studio];
+  home = {
+    packages = [pkgs.obs-studio];
+    file.".local/share/applications/com.obsproject.Studio.desktop".text = ''
+      [Desktop Entry]
+      Version=1.0
+      Name=OBS Studio
+      GenericName=Streaming/Recording Software
+      Comment=Free and Open Source Streaming/Recording Software
+      Exec=env QT_QPA_PLATFORM=xcb obs
+      Icon=com.obsproject.Studio
+      Terminal=false
+      Type=Application
+      Categories=AudioVideo;Recorder;
+      StartupNotify=true
+      StartupWMClass=obs
+    '';
+  };
 }
