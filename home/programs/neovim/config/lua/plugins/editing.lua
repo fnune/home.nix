38e0c1a900c9a1bb94f4376be2dc3f2699d8157c
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-12-20 15:13:29 +0100

    Move plugins out of editing.lua that are not about editing

--- home/programs/neovim/config/lua/plugins/editing.lua
+++ home/programs/neovim/config/lua/plugins/editing.lua
@@ -1,7 +1,5 @@
 return {
-  { "LunarVim/bigfile.nvim" },
   { "backdround/improved-ft.nvim", opts = { use_default_mappings = true } },
-  { "christoomey/vim-tmux-navigator" },
   { "machakann/vim-swap" },
   { "matze/vim-move" },
   { "tpope/vim-eunuch" },
