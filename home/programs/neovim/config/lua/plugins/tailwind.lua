23603189b32e1db2e6db75ce3a3bf4076e9b6773
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-01-07 16:30:16 +0100

    Disable tailwind autosort, use biome code action, as they differ

--- home/programs/neovim/config/lua/plugins/tailwind.lua
+++ home/programs/neovim/config/lua/plugins/tailwind.lua
@@ -9,16 +9,6 @@ return {
   opts = {
     document_color = { enabled = false },
     conceal = { enabled = false },
-    server = {
-      override = true,
-      on_attach = function(_, bufnr)
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          buffer = bufnr,
-          callback = function()
-            vim.cmd("TailwindSortSync")
-          end,
-        })
-      end,
-    },
+    server = { override = true },
   },
 }
