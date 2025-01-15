ad1ff56997db4fb486811f73caa07062186196c4
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-01-15 12:21:59 +0100

    Use unstable dbgate

--- home/programs/dbgate/dbgate.nix
+++ home/programs/dbgate/dbgate.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: let
-  inherit (pkgs) dbgate;
+  inherit (pkgs.unstable) dbgate;
   dbgate-icon = pkgs.fetchurl {
     url = "https://github.com/dbgate/dbgate/raw/35e9ff607d3014e0cb55f07d2705663581c03db2/app/icon.png";
     sha256 = "0ii15gj2rnf9nyw20b11s5nlwzv4qnpb1dllig186zc27kv0l1nn";
