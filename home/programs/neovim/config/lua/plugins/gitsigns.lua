3f8afd6f5e9479f21dd538f3067ac86bf93402fb
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-01-18 10:51:02 +0100

    Use <leader>JK to navigate to the next/previous gitsigns hunk

--- home/programs/neovim/config/lua/plugins/gitsigns.lua
+++ home/programs/neovim/config/lua/plugins/gitsigns.lua
@@ -1,1 +1,20 @@
-return { "lewis6991/gitsigns.nvim", config = true }
+return {
+  "lewis6991/gitsigns.nvim",
+  config = true,
+  keys = {
+    {
+      "<leader>J",
+      function()
+        require("gitsigns").nav_hunk("next")
+      end,
+      desc = "Navigate to the next hunk",
+    },
+    {
+      "<leader>K",
+      function()
+        require("gitsigns").nav_hunk("prev")
+      end,
+      desc = "Navigate to the previous hunk",
+    },
+  },
+}
