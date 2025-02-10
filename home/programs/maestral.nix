6b6404ff301f518d2d6a8c65a2234da4c59c7993
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-02-10 09:52:19 +0100

    Remove maestral service, use maestral autostart -Y

--- home/programs/maestral.nix
+++ home/programs/maestral.nix
@@ -1,21 +1,3 @@
-{pkgs, ...}: let
-  inherit (pkgs) maestral;
-in {
-  home.packages = [maestral];
-
-  systemd.user.services.maestral = {
-    Unit.Description = "Maestral daemon";
-    Service = {
-      Type = "notify";
-      NotifyAccess = "exec";
-      ExecStart = "${maestral}/bin/maestral start -f";
-      ExecStop = "${maestral}/bin/maestral stop";
-      ExecStopPost = ''
-        /usr/bin/env bash -c "if [ $${SERVICE_RESULT} != success ]; \
-        then notify-send Maestral 'Daemon failed'; fi"
-      '';
-      WatchdogSec = "30s";
-    };
-    Install.WantedBy = ["default.target"];
-  };
+{pkgs, ...}: {
+  home.packages = with pkgs.unstable; [maestral];
 }
