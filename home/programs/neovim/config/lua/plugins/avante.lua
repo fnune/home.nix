744757770e7ca4d00e0b54bec95fe2bb08683f2a
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-03-23 16:54:35 +0100

    Make standard work better with Avante highlights

--- home/programs/neovim/config/lua/plugins/avante.lua
+++ home/programs/neovim/config/lua/plugins/avante.lua
@@ -10,33 +10,42 @@ return {
     "nvim-tree/nvim-web-devicons",
     "stevearc/dressing.nvim",
   },
-  opts = {
-    provider = "claude",
-    hints = { enabled = false },
-    windows = { ask = { start_insert = false } },
-    file_selector = {
-      provider = "telescope",
-      provider_opts = {
-        get_filepaths = function(params)
-          local cwd = params.cwd
-          local selected_filepaths = params.selected_filepaths
+  config = function()
+    require("avante").setup({
+      provider = "claude",
+      hints = { enabled = false },
+      windows = { ask = { start_insert = false } },
+      file_selector = {
+        provider = "telescope",
+        provider_opts = {
+          get_filepaths = function(params)
+            local cwd = params.cwd
+            local selected_filepaths = params.selected_filepaths
 
-          local cmd = string.format("rg --files --hidden --follow '%s'", vim.fn.fnameescape(cwd))
-          local output = vim.fn.system(cmd)
+            local cmd = string.format("rg --files --hidden --follow '%s'", vim.fn.fnameescape(cwd))
+            local output = vim.fn.system(cmd)
 
-          local filepaths = vim.split(output, "\n", { trimempty = true })
-          filepaths = vim.tbl_map(function(path)
-            return path:sub(#cwd + 2) -- +2 to account for the trailing slash
-          end, filepaths)
+            local filepaths = vim.split(output, "\n", { trimempty = true })
+            filepaths = vim.tbl_map(function(path)
+              return path:sub(#cwd + 2) -- +2 to account for the trailing slash
+            end, filepaths)
 
-          return vim
-            .iter(filepaths)
-            :filter(function(filepath)
-              return not vim.tbl_contains(selected_filepaths, filepath)
-            end)
-            :totable()
-        end,
+            return vim
+              .iter(filepaths)
+              :filter(function(filepath)
+                return not vim.tbl_contains(selected_filepaths, filepath)
+              end)
+              :totable()
+          end,
+        },
       },
-    },
-  },
+    })
+
+    vim.cmd([[
+      hi! link AvanteConflictCurrent DiffDelete
+      hi! link AvanteConflictCurrentLabel NeogitDiffDeleteCursor
+      hi! link AvanteConflictIncoming DiffAdd
+      hi! link AvanteConflictIncomingLabel NeogitDiffAddCursor
+    ]])
+  end,
 }
