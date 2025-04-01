921d46989dc137a2d6db462eb8959e43e372cf7d
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-04-01 14:43:28 +0200

    Disable printer autodiscovery to close port

--- os/printing.nix
+++ os/printing.nix
@@ -2,10 +2,5 @@ _: {
   services = {
     printing.enable = true;
     system-config-printer.enable = true;
-    avahi = {
-      enable = true;
-      nssmdns4 = true;
-      openFirewall = true;
-    };
   };
 }
