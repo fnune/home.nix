0b7e47569819284410ef9fa46dcfd58e1585fcb4
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-04-04 10:27:53 +0200

    Improve my avante config

--- home/programs/neovim/config/lua/plugins/avante.lua
+++ home/programs/neovim/config/lua/plugins/avante.lua
@@ -7,12 +7,26 @@ return {
     "MunifTanjim/nui.nvim",
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
+    "stevearc/dressing.nvim",
   },
   config = function()
     require("avante").setup({
       provider = "claude",
       hints = { enabled = false },
+      behavior = { enable_claude_text_editor_tool_mode = true },
       windows = { ask = { start_insert = false } },
+      system_prompt = [[
+        Important user preferences:
+
+        - Never add or remove code comments unless I ask for it. Instead, make
+          your code self-explanatory using good variable names and semantics.
+        - If the language allows it, make full use of static typing and
+          inference.
+        - If you need to provide a summary of what we've done, make it
+          extremely concise regardless of what you've been asked to do before.
+        - Never spend any words to say why your solution is good at the end of
+          your response.
+      ]],
       file_selector = {
         provider = "snacks",
         provider_opts = {
