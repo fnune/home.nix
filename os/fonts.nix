32013cfe2b81c279902337240fc21f63114e419d
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-04-04 09:39:07 +0200

    Put FREETYPE_PROPERTIES in the correct place

--- os/fonts.nix
+++ os/fonts.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   # https://blog.aktsbot.in/no-more-blurry-fonts.html
-  environment.variables = {
+  environment.sessionVariables = {
     FREETYPE_PROPERTIES = "cff:no-stem-darkening=0 autofitter:no-stem-darkening=0";
   };
   fonts = {
