7f6261d43a6f02c050bcc51f2907a0a5c02adf93
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-04-06 11:37:58 +0200

    Exclude more wallpapers I don't like

--- home/desktop/wallpapers.nix
+++ home/desktop/wallpapers.nix
@@ -14,14 +14,21 @@
     "a_close_up_of_rocks.jpg"
     "a_iceberg_in_the_water_with_mountains_in_the_background.jpg"
     "a_mountain_range_with_snow_on_top.jpeg"
+    "a_mountain_range_with_snow_on_top_01.jpg"
     "a_mountain_with_a_road_going_through_it.jpg"
     "a_mountain_with_clouds_above_it.png"
     "a_mountain_with_snow_and_clouds_01.jpg"
+    "a_mountain_with_snow_on_top_01.jpg"
+    "a_person_on_a_cliff.jpg"
     "a_river_in_a_snowy_landscape.jpg"
+    "a_rocky_island_with_waves_crashing_on_it.jpg"
     "a_rocky_island_with_waves_crashing_on_it.png"
     "a_silhouette_of_trees_on_a_hill.jpg"
     "a_small_waterfall_in_a_grassy_area.jpg"
+    "a_snowy_mountain_tops_with_blue_sky.jpg"
     "a_snowy_mountain_tops_with_clouds_in_the_background.png"
+    "a_tree_in_a_body_of_water_with_mountains_in_the_background.jpg"
+    "a_waterfall_in_the_snow.jpg"
   ];
   wallpapers = pkgs.runCommand "filter-wallpapers" {} ''
     cp -r ${source}/mountain $out
