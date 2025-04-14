7d7b93d8f5efd5a68318d88360ae975c90695598
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-04-14 11:44:51 +0200

    Enable geoclue2 using beacondb

--- os/region-language.nix
+++ os/region-language.nix
@@ -8,6 +8,12 @@ in {
   console.keyMap = layout;
   services = {
     automatic-timezoned.enable = true;
+    geoclue2 = {
+      enable = true;
+      # https://github.com/NixOS/nixpkgs/pull/391845
+      geoProviderUrl = "https://api.beacondb.net/v1/geolocate";
+      submissionUrl = "https://api.beacondb.net/v2/geosubmit";
+    };
     xserver.xkb = {
       inherit layout;
       variant = "";
