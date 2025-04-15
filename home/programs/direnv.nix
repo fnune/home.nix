7128e9314f4ebb7dcb8d950de48291c80d8a4a3e
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-04-15 10:50:37 +0200

    Upgrade the system and Neovim plugins

--- home/programs/direnv.nix
+++ home/programs/direnv.nix
@@ -2,9 +2,9 @@ _: {
   programs.direnv = {
     enable = true;
     enableZshIntegration = true;
+    silent = true;
   };
   home.sessionVariables = {
-    DIRENV_LOG_FORMAT = "";
     DIRENV_WARN_TIMEOUT = "10000h";
   };
 }
