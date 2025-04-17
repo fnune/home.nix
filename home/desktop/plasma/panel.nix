66c9357addc5f9262250506880222017645b61c1
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-04-17 14:18:10 +0200

    Use a larger panel clock font size

--- home/desktop/plasma/panel.nix
+++ home/desktop/plasma/panel.nix
@@ -83,7 +83,7 @@
               dateDisplayFormat = "BesideTime";
               dateFormat = "longDate";
               fontFamily = config.fontconfig.sans;
-              fontSize = 12;
+              fontSize = 14;
               fontStyleName = "Medium";
               fontWeight = 500;
               selectedTimeZones = "Local,America/New_York,America/Los_Angeles";
