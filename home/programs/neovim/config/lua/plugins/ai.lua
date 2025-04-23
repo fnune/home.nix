d242520c57f33b668b05349533f54b37440b6390
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-04-23 20:39:36 +0200

    Use upstream version of magenta.nvim after feature merge

--- home/programs/neovim/config/lua/plugins/ai.lua
+++ home/programs/neovim/config/lua/plugins/ai.lua
@@ -1,7 +1,6 @@
 return {
-  "fnune/magenta.nvim",
+  "dlants/magenta.nvim",
   lazy = false,
-  branch = "fnune/add-support-snacks",
   build = "npm install --frozen-lockfile",
   opts = {
     picker = "snacks",
