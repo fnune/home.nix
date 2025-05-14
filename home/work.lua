a74e43d7190b7c98039aed7dc18ab735c1354d9c
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-05-14 10:14:10 +0200

    Add work-specific Neovim config

---
+++ home/work.lua
@@ -0,0 +1,14 @@
+local neotest = require("neotest")
+
+neotest.setup_project(vim.loop.cwd(), {
+  adapters = {
+    require("neotest-golang")({
+      env = {
+        PULUMI_LOCAL_DB = "true",
+        PULUMI_LOCAL_DATABASE_ENDPOINT = "localhost:3307",
+        PULUMI_DATABASE_NAME = "pulumi",
+        PULUMI_INSIGHTS_LOCAL_OBJECTS = "/tmp/pulumi-tests-insights-local-objects",
+      },
+    }),
+  },
+})
--- home/work.nix
+++ home/work.nix
@@ -1,9 +1,21 @@
-{pkgs, ...}: {
-  home.packages = with pkgs.unstable; [
-    _1password-gui
-    awscli2
-    pulumi-esc
-    slack
-    zoom-us
-  ];
+{
+  pkgs,
+  config,
+  ...
+}: let
+  service = "${config.home.homeDirectory}/Development/pulumi-service";
+in {
+  home = {
+    packages = with pkgs.unstable; [
+      _1password-gui
+      awscli2
+      go
+      mysql80
+      pulumi-esc
+      slack
+      zoom-us
+    ];
+
+    file."${service}/.nvim.lua".source = ./work.lua;
+  };
 }
