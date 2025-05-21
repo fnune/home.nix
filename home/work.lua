6657f6f293f87a8a3d7746b12f844b299b0c98a2
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-05-21 14:12:06 +0200

    Set up Neotest for integration tests

--- home/work.lua
+++ home/work.lua
@@ -1,14 +1,13 @@
 local neotest = require("neotest")
 
+local tags = "-tags=integration"
+
 neotest.setup_project(vim.loop.cwd(), {
   adapters = {
     require("neotest-golang")({
-      env = {
-        PULUMI_LOCAL_DB = "true",
-        PULUMI_LOCAL_DATABASE_ENDPOINT = "127.0.0.1:3307",
-        PULUMI_DATABASE_NAME = "pulumi",
-        PULUMI_INSIGHTS_LOCAL_OBJECTS = "/tmp/pulumi-tests-insights-local-objects",
-      },
+      go_test_args = { "-v", "-race", "-count=1", tags },
+      go_list_args = { tags },
+      dap_go_opts = { delve = { build_flags = { tags } } },
     }),
   },
 })
