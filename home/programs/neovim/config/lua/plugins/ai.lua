407465b41ab7559bb31f43fb20388e0994ae1be9
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-06-05 17:12:31 +0200

    Add a keybinding to toggle CodeCompanion

--- home/programs/neovim/config/lua/plugins/ai.lua
+++ home/programs/neovim/config/lua/plugins/ai.lua
@@ -4,6 +4,14 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
   },
+  keys = {
+    {
+      "<leader>at",
+      "<cmd>CodeCompanionChat Toggle<CR>",
+      desc = "Toggle the AI chat",
+      silent = true,
+    },
+  },
   opts = {
     strategies = {
       chat = {
