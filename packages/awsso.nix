68af6f177124994be99713505984d9a5788c292e
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-06-17 09:45:08 +0200

    Run aws sso login only if I don't have an existing session

--- packages/awsso.nix
+++ packages/awsso.nix
@@ -1,5 +1,12 @@
 {pkgs, ...}:
 pkgs.writeShellScriptBin "awsso" ''
+  if aws sts get-caller-identity >/dev/null 2>&1; then
+    echo "AWS session is already active"
+    aws sts get-caller-identity
+    exit 0
+  fi
+
+  echo "No active AWS session found, starting SSO login..."
   aws sso login 2>&1 | while IFS= read -r line; do
     echo "$line"
 
