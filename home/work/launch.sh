49896446d3e9fd67818f2681517f93640a10fe68
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-06-26 15:25:06 +0200

    Add HMR setup to launch.sh

--- home/work/launch.sh
+++ home/work/launch.sh
@@ -13,8 +13,17 @@ function t() {
     tmux split-window -c "$service_repo" -h
     tmux send-keys "awsso" C-m
 
+    # Local database
     tmux split-window -c "$service_repo" -v
     tmux send-keys "make ensure && ./scripts/local-mysql.sh up" C-m
+
+    # HMR + tunneled backend setup
+    tmux split-window -c "$service_repo" -h
+    tmux send-keys "esc run pulumi/default/review-stacks -- kubectl port-forward svc/pulumi-api 8080:8080 -n \$PULUMI_STACK_NAME_OVERRIDE" C-m
+    tmux split-window -c "$service_repo" -v
+    tmux send-keys "PULUMI_CONSOLE_DOMAIN=localhost:4200 PULUMI_API=http://localhost:8080 ./scripts/dev/run-console.sh backend" C-m
+    tmux split-window -c "$service_repo/cmd/console2" -h
+    tmux send-keys "PULUMI_CONSOLE_DOMAIN=localhost:4200 PULUMI_API=http://localhost:8080 yarn run start:hmr" C-m
 
     tmux select-pane -t 0
 
