d3c5b6fb7ece0d1c9b7a751f98bf66a3835a0bc7
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-07-27 20:59:59 +0200

    Enable flatpaks

--- home/programs/flatpaks.nix
+++ home/programs/flatpaks.nix
@@ -1,10 +1,13 @@
 _: {
-  services.flatpak.packages = [
-    "com.slack.Slack"
-    "com.spotify.Client"
-    "com.yubico.yubioath"
-    "org.kde.krita"
-    "org.signal.Signal"
-    "us.zoom.Zoom"
-  ];
+  services.flatpak = {
+    enable = true;
+    packages = [
+      "com.slack.Slack"
+      "com.spotify.Client"
+      "com.yubico.yubioath"
+      "org.kde.krita"
+      "org.signal.Signal"
+      "us.zoom.Zoom"
+    ];
+  };
 }
