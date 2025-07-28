42e4e2f643c33550ccda462fc65affa1c9eb26d0
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-07-28 12:47:40 +0200

    Remove special GIT_SSH_COMMAND

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -5,9 +5,6 @@
 }: {
   home = {
     packages = with pkgs; [difftastic diffstat];
-    sessionVariables = {
-      GIT_SSH_COMMAND = "ssh -i ${config.home.homeDirectory}/.ssh/id_ed25519 -o IdentitiesOnly=yes";
-    };
   };
   programs.zsh = {
     shellAliases = {
