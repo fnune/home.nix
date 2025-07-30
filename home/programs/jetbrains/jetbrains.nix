a269777716e0f57f36425830c67cbdfd5b4066a4
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-07-30 09:12:13 +0200

    Move jetbrains flatpak installs to where they belong

--- home/programs/jetbrains/jetbrains.nix
+++ home/programs/jetbrains/jetbrains.nix
@@ -1,4 +1,9 @@
 {config, ...}: {
+  services.flatpak.packages = [
+    "com.jetbrains.DataGrip"
+    "com.jetbrains.GoLand"
+  ];
+
   home = {
     file."${config.home.homeDirectory}/.ideavimrc".source = ./ideavimrc;
   };
