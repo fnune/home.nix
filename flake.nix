e13609485d9a90c8f31783e5307ea7e38a04e9a3
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-08-02 15:09:42 +0200

    Add unstable channel

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -19,12 +20,19 @@
 
   outputs = {
     nixpkgs,
+    nixpkgs-unstable,
     home-manager,
     plasma-manager,
     nix-flatpak,
     ...
   }: let
     system = "x86_64-linux";
+    nixpkgsOverlay = final: prev: {
+      unstable = import nixpkgs-unstable {
+        inherit (prev) config;
+        inherit system;
+      };
+    };
   in {
     defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
 
@@ -33,7 +41,10 @@
       nixFlatpak = nix-flatpak.homeManagerModules.nix-flatpak;
     in {
       fausto = home-manager.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs {inherit system;};
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [nixpkgsOverlay];
+        };
         modules = [./home/home.nix nixFlatpak plasmaManager];
       };
     };
