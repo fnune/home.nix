ff9e8eb6f5aaa8ea5b1f74a25374f4770788cdfd
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-08-02 17:53:00 +0200

    Install Jetbrains tools using jetbrains-toolbox

--- home/programs/jetbrains/jetbrains.nix
+++ home/programs/jetbrains/jetbrains.nix
@@ -1,10 +1,6 @@
-{config, ...}: {
-  services.flatpak.packages = [
-    "com.jetbrains.DataGrip"
-    "com.jetbrains.GoLand"
-  ];
-
+{pkgs, config, ...}: {
   home = {
     file."${config.home.homeDirectory}/.ideavimrc".source = ./ideavimrc;
+    packages = [pkgs.jetbrains-toolbox];
   };
 }
