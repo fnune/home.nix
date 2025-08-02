be2c31a8469faf3ccf104a400f494b3f4e1fcb95
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-08-02 10:51:18 +0200

    Remove some utils likely to already be in base img or unneeded

--- home/programs/networking.nix
+++ home/programs/networking.nix
@@ -2,15 +2,12 @@
   services.apt.packages = ["wireshark"];
 
   home.packages = with pkgs; [
-    curl
     dig
     inetutils
     nettools
     nmap
-    openssl
     sshfs
     traceroute
-    wget
     whois
   ];
 }
--- home/programs/utils.nix
+++ home/programs/utils.nix
@@ -1,27 +1,14 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    ddcutil
-    dmidecode
-    du-dust
     exiftool
     fd
     ffmpeg
-    file
     gcc
     gnumake
-    gzip
     jq
-    killall
     lm_sensors
     lshw
-    lsof
-    man-pages
-    man-pages-posix
     moreutils
-    pciutils
-    tree
-    unzip
-    util-linux
     watchexec
     wl-clipboard
   ];
