130357cca6c1692bb33b06623a2beb6906a0d63c
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-08-03 13:27:23 +0200

    Add fontconfig aliases

--- home/desktop/fonts.nix
+++ home/desktop/fonts.nix
@@ -25,4 +25,39 @@
       };
     };
   };
+
+  xdg.configFile = {
+    "fontconfig/conf.d/60-hm-aliases.conf".text = let
+      sansAliases = [
+        "-apple-system"
+        "Helvetica"
+        "Inter"
+      ];
+      monoAliases = [
+        "DejaVu Sans Mono"
+        "Liberation Mono"
+        "SF Mono"
+        "SFMono-Regular"
+      ];
+      makeAliases = aliases: targetFont:
+        builtins.concatStringsSep "\n" (
+          map (alias: ''
+            <alias binding="strong">
+              <family>${alias}</family>
+              <prefer>
+                <family>${targetFont}</family>
+              </prefer>
+            </alias>
+          '')
+          aliases
+        );
+    in ''
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+      <fontconfig>
+        ${makeAliases sansAliases config.fontconfig.sans}
+        ${makeAliases monoAliases config.fontconfig.mono}
+      </fontconfig>
+    '';
+  };
 }
