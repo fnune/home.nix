d04636852047c2a631fb2b14deb73c1934fa4095
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-08-04 03:50:46 +0200

    Set up markdown, Obsidian, and restore oil

---
+++ home/programs/neovim/config/lua/plugins/markdown.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+    opts = {
+      checkbox = { checked = { icon = "" }, unchecked = { icon = "󰄱" } },
+      code = { style = "normal", border = "thin", above = "─", below = "─" },
+      heading = { enabled = false },
+      indent = { enabled = false },
+      pipe_table = { style = "normal" },
+      sign = { enabled = false },
+      file_types = { "markdown", "codecompanion" },
+      overrides = {
+        buftype = {
+          nofile = {
+            code = { border = "hide" },
+          },
+        },
+      },
+    },
+  },
+}
