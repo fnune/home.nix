482c3416ae3a075785e0e0deaa6a29189ecc47f9
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-08-06 16:44:31 +0200

    Add GOTOOLCHAIN="local" so Goland does not get confused

--- home/work/envrc.sh
+++ home/work/envrc.sh
@@ -4,6 +4,7 @@ export AWS_PROFILE="pulumi-service-dev-stacks"
 export CHROME_BIN="$(which chromium)"
 export GOPATH="$HOME/go"
 export GOPRIVATE="github.com/pulumi"
+export GOTOOLCHAIN="local"
 export PATH="$GOPATH/bin:$PATH"
 export PULUMI_STACK_NAME_OVERRIDE=fnune-review
 
