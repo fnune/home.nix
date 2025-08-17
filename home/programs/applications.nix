e169d9f2ced432d9edf8d1fcf79e6fe0f57dff69
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-08-17 11:51:28 +0200

    Revert "Remove Slack flatpak, install by downloading .deb"
    This reverts commit 38d00936f1b521b7d02a1dda597d7012aa50c688.
    
    Installing Slack via apt brings in gnome-keyring, breaking pinentry.

--- home/programs/applications.nix
+++ home/programs/applications.nix
@@ -8,6 +8,7 @@ _: {
     packages = [
       "com.dropbox.Client"
       "com.obsproject.Studio"
+      "com.slack.Slack"
       "com.spotify.Client"
       "com.yubico.yubioath"
       "io.podman_desktop.PodmanDesktop"
