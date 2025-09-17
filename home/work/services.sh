747bb7ca2950007e8172a9d29a827ef498cf7ad9
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-09-17 15:12:41 +0200

    Improve output of services.sh

--- home/work/services.sh
+++ home/work/services.sh
@@ -2,6 +2,12 @@
 
 set -euo pipefail
 
+run_and_log() {
+  local service_name="$1"
+  shift
+  "$@" > >(sed "s/^/[${service_name}] /") 2> >(sed "s/^/[${service_name} (?)] /" >&2)
+}
+
 cleanup() {
   echo "Stopping all processes..."
   kill "$(jobs -p)" 2>/dev/null
@@ -11,11 +17,12 @@ cleanup() {
 
 trap cleanup SIGINT SIGTERM
 
-echo "Starting port-forwards..."
-esc run pulumi/default/review-stacks -- kubectl port-forward svc/pulumi-api 8080:8080 -n "$PULUMI_STACK_NAME_OVERRIDE" &
-esc run pulumi/default/review-stacks -- kubectl port-forward pod/mysql-0 3308:3306 -n "$PULUMI_STACK_NAME_OVERRIDE" &
+run_and_log "pulumi-api-pf" echo "Starting API port-forwards..."
+run_and_log "pulumi-api-pf" esc run pulumi/default/review-stacks -- kubectl port-forward svc/pulumi-api 8080:8080 -n "$PULUMI_STACK_NAME_OVERRIDE" &
+run_and_log "mysql-pf" echo "Starting MySQL port-forwards..."
+run_and_log "mysql-pf" esc run pulumi/default/review-stacks -- kubectl port-forward pod/mysql-0 3308:3306 -n "$PULUMI_STACK_NAME_OVERRIDE" &
 
 echo "Starting local database..."
-make ensure && make install_gotools && ./scripts/local-mysql.sh up &
+run_and_log "local-db" bash -c "make ensure && make install_gotools && ./scripts/local-mysql.sh up" &
 
 wait
