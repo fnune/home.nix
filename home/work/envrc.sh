1918da151928d046c063ee95445964baa26450a3
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-10-18 22:07:58 +0200

    Use a hidden directory for GOPATH

--- home/work/envrc.sh
+++ home/work/envrc.sh
@@ -2,7 +2,7 @@
 
 export AWS_PROFILE="pulumi-service-dev-stacks"
 export CHROME_BIN="$(which chromium)"
-export GOPATH="$HOME/go"
+export GOPATH="$HOME/.go"
 export GOPRIVATE="github.com/pulumi"
 export HELPMAKEGO_EXPERIMENT_DAEMON=true
 export PATH="$GOPATH/bin:$PATH"
