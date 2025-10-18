01c77dea92694e37a9fe5e4057816f456729b953
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-10-18 22:55:26 +0200

    Fix GOPATH for pu/pu

--- home/work/envrc.oss.sh
+++ home/work/envrc.oss.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export GOPATH="$HOME/go"
+export GOPATH="$HOME/.go"
 export GOPRIVATE="github.com/pulumi"
 export PATH="$GOPATH/bin:$PATH"
 export PULUMI_TEST_ORG=pulumi_local
