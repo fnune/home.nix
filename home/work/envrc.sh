5c70f75a90e42bbea7ca3c28ed86182193fb615a
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-10-18 23:43:21 +0200

    Set MYSQL_DATA_PATH

--- home/work/envrc.sh
+++ home/work/envrc.sh
@@ -7,6 +7,7 @@ export GOPRIVATE="github.com/pulumi"
 export HELPMAKEGO_EXPERIMENT_DAEMON=true
 export PATH="$GOPATH/bin:$PATH"
 export PULUMI_SERVICE_LOCAL_DATA="$HOME/.pulumi-service-local/data"
+export MYSQL_DATA_PATH="$PULUMI_SERVICE_LOCAL_DATA"
 export PULUMI_STACK_NAME_OVERRIDE=fnune-review
 
 dotenv shared.env
