409691d9250438e8b8d6a24237b2060dfd948e31
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-11-07 11:12:38 +0100

    Open either GoLand or $EDITOR from lazygit depending on project

--- home/programs/lazygit.nix
+++ home/programs/lazygit.nix
@@ -1,5 +1,55 @@
-{config, ...}: let
+{
+  config,
+  pkgs,
+  ...
+}: let
   jetbrainsTool = config.jetbrains.tool;
+
+  openInJetbrainsScript = pkgs.writeShellScript "open-in-jetbrains" ''
+    PROJECT_DIR="$(git rev-parse --show-toplevel 2>/dev/null || pwd)"
+    ${jetbrainsTool} "$PROJECT_DIR" "$@" >/dev/null 2>&1 &
+    disown
+  '';
+
+  openInNvimScript = pkgs.writeShellScript "open-in-nvim" ''
+    LINE=""
+    FILENAME=""
+
+    while [ $# -gt 0 ]; do
+      case "$1" in
+        --line)
+          LINE="$2"
+          shift 2
+          ;;
+        *)
+          FILENAME="$1"
+          shift
+          ;;
+      esac
+    done
+
+    if [ -n "$LINE" ]; then
+      exec ''${EDITOR:-nvim} "+$LINE" "$FILENAME"
+    else
+      exec ''${EDITOR:-nvim} "$FILENAME"
+    fi
+  '';
+
+  openInEditorScript = pkgs.writeShellScript "open-in-editor" ''
+    PROJECT_DIR="$(git rev-parse --show-toplevel 2>/dev/null || pwd)"
+
+    if [ -d "$PROJECT_DIR/.idea" ]; then
+      exec ${openInJetbrainsScript} "$@"
+    else
+      printf "No .idea folder found. Open in JetBrains IDE? (Y=JetBrains, n=\$EDITOR): " >&2
+      read -r CHOICE
+      if [ "$CHOICE" = "n" ] || [ "$CHOICE" = "N" ]; then
+        exec ${openInNvimScript} "$@"
+      else
+        exec ${openInJetbrainsScript} "$@"
+      fi
+    fi
+  '';
 in {
   programs = {
     lazygit = {
@@ -7,11 +57,11 @@ in {
       settings = {
         promptToReturnFromSubprocess = false;
         os = {
-          edit = "${jetbrainsTool} {{filename}}";
-          editAtLine = "${jetbrainsTool} --line {{line}} {{filename}}";
-          editAtLineAndWait = "${jetbrainsTool} --line {{line}} {{filename}} --wait";
-          open = "${jetbrainsTool} {{filename}}";
-          openDirInEditor = "${jetbrainsTool} {{filename}}";
+          edit = "${openInEditorScript} {{filename}}";
+          editAtLine = "${openInEditorScript} --line {{line}} {{filename}}";
+          editAtLineAndWait = "${openInEditorScript} --line {{line}} {{filename}}";
+          open = "${openInEditorScript} {{filename}}";
+          openDirInEditor = "${openInEditorScript}";
         };
         git = {
           overrideGpg = true;
