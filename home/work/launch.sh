4f776aa59d2f02c6f77798c7513fb7785ecd7f65
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-11-08 12:45:36 +0100

    Add a status line to tmux at the top, session-specific

--- home/work/launch.sh
+++ home/work/launch.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/env bash
 
+function set_tmux_session_theme() {
+  local session="$1"
+  local bg_color="$2"
+  local fg_color="#ffffff"
+
+  tmux set -t "$session" status-style "bg=$bg_color,fg=$fg_color"
+  tmux set -t "$session" status-left " #S "
+  tmux set -t "$session" status-right ""
+}
+
 function t() {
   local repos="$HOME/Development/pulumi"
   local service_repo="$repos/pulumi-service"
   local pulumi_repo="$repos/pulumi"
+
+  local theme_light_blue="#5694ca"
+  local theme_dark_blue="#003078"
+  local theme_green="#00703c"
+  local theme_dark_green="#1d2d13"
+  local theme_yellow="#ffdd00"
+  local theme_white="#ffffff"
 
   cd "$service_repo" || exit
 
@@ -13,6 +30,7 @@ function t() {
   if ! tmux list-sessions | grep -q "^pulumi-service:"; then
     tmux new-session -c "$service_repo" -d -s pulumi-service -n authorization
     eval "$env_script"
+    set_tmux_session_theme pulumi-service "$theme_light_blue"
 
     tmux new-window -c "$service_repo" -n auxiliary
 
@@ -38,8 +56,10 @@ function t() {
     tmux send-keys -t pulumi-service:git "lazygit" C-m
     tmux kill-window -t authorization
   fi
+
   if ! tmux list-sessions | grep -q "^pulumi/pulumi:"; then
-    tmux new-session -c "$pulumi_repo" -d -s "pulumi/pulumi:" -n main
+    tmux new-session -c "$pulumi_repo" -d -s "pulumi/pulumi" -n main
+    set_tmux_session_theme "pulumi/pulumi" "$theme_green"
     tmux send-keys -t "pulumi/pulumi:main" "lazygit" C-m
   fi
   tmux attach-session -t pulumi-service
