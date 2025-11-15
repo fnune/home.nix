e2c9ec29b22d76bdfbb353f0fbbc4d0fd83921ba
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-11-15 16:50:09 +0100

    Move location of notes dir

--- home/personal/launch.sh
+++ home/personal/launch.sh
@@ -3,7 +3,7 @@
 source "$HOME/.home.nix/home/programs/tmux/tmux-utils.sh"
 
 function n() {
-  local notes_dir="$HOME/Documents/Fausto"
+  local notes_dir="$HOME/pCloudDrive/Documents/Notes"
   local dotfiles_dir="$HOME/.home.nix"
 
   local session="📝 personal"
