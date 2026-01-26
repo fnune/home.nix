46ab203fdedecd808824e15c272e2c5b3b40e24f
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2026-01-26 07:03:34 +0100

    Upgrade packages

--- home/programs/neovim/neovim.nix
+++ home/programs/neovim/neovim.nix
@@ -3,61 +3,55 @@
   config,
   ...
 }: let
-  neovim = pkgs.neovim-unwrapped;
-in {
-  programs = {
-    neovim = {
-      enable = true;
-      package = neovim;
-      extraLuaPackages = ps: [ps.magick];
-      extraPackages = [pkgs.imagemagick];
-      withNodeJs = true;
-      withPython3 = true;
-      withRuby = true;
-    };
+  neovim = pkgs.wrapNeovim pkgs.neovim-unwrapped {
+    withNodeJs = true;
+    withPython3 = true;
+    withRuby = true;
+    extraLuaPackages = ps: [ps.magick];
   };
-
+in {
   home = {
-    packages = with pkgs; [
-      # LSPs
-      angular-language-server
-      basedpyright
-      bash-language-server
-      gopls
-      lua-language-server
-      nil
-      pyright
-      rustup
-      stylelint-lsp
-      taplo
-      terraform-ls
-      typescript-language-server
-      vscode-langservers-extracted
-      yaml-language-server
-      # Formatters
-      alejandra
-      biome
-      gofumpt
-      nodePackages.prettier
-      prettierd
-      python3Packages.black
-      shfmt
-      stylua
-      # Debuggers
-      delve
-      python3Packages.debugpy
-      # Lower-level tools
-      inotify-tools
-      nodejs_22
-      sqlite
-      tree-sitter
-    ];
+    packages =
+      [
+        neovim
+      ]
+      ++ (with pkgs; [
+        # LSPs
+        angular-language-server
+        basedpyright
+        bash-language-server
+        gopls
+        lua-language-server
+        nil
+        pyright
+        rustup
+        stylelint-lsp
+        taplo
+        terraform-ls
+        typescript-language-server
+        vscode-langservers-extracted
+        yaml-language-server
+        # Formatters
+        alejandra
+        biome
+        gofumpt
+        nodePackages.prettier
+        prettierd
+        python3Packages.black
+        shfmt
+        stylua
+        # Debuggers
+        delve
+        python3Packages.debugpy
+        # Lower-level tools
+        imagemagick
+        inotify-tools
+        nodejs_22
+        sqlite
+        tree-sitter
+      ]);
 
-    file = {
-      ".config/nvim/" = {
-        source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.home.nix/home/programs/neovim/config";
-      };
-    };
+    file.".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.home.nix/home/programs/neovim/config";
 
     sessionVariables = {
       EDITOR = "${neovim}/bin/nvim";
