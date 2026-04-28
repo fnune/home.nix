333228c53c5cfef6a31bc761e61e8348637da8d3
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2026-04-28 12:01:36 +0200

    Group and annotate Neovim settings

--- home/programs/neovim/config/lua/settings.lua
+++ home/programs/neovim/config/lua/settings.lua
@@ -1,51 +1,69 @@
 local constants = require("constants")
 
 vim.cmd("syntax enable")
-vim.cmd("syntax sync minlines=10000")
-
+vim.cmd("syntax sync minlines=10000") -- fixes broken highlighting in long files
 vim.cmd("filetype plugin indent on")
-vim.cmd("set ignorecase smartcase")
-vim.opt.autoindent = true
-vim.opt.autoread = true
-vim.opt.breakindent = true
-vim.opt.breakindentopt = "list:-1"
-vim.opt.cmdheight = 2
-vim.opt.cursorline = true
+
 vim.opt.encoding = "utf-8"
-vim.opt.expandtab = true
-vim.opt.exrc = true
-vim.opt.hidden = true
-vim.opt.laststatus = 3
-vim.opt.hlsearch = true
-vim.opt.incsearch = true
-vim.opt.lazyredraw = true
-vim.opt.linebreak = true
-vim.opt.list = true
-vim.opt.listchars = constants.listchars.normal
-vim.opt.number = true
-vim.opt.redrawtime = 10000
-vim.opt.relativenumber = false
-vim.opt.scrolloff = 6
-vim.opt.secure = true
-vim.opt.shiftwidth = 2
-vim.opt.shortmess = vim.opt.shortmess + "Ic"
-vim.opt.showcmd = true
-vim.opt.showmatch = true
-vim.opt.showmode = false
-vim.opt.signcolumn = "yes"
+
+-- Indentation
+vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.smarttab = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+
+-- Search
+vim.cmd("set ignorecase smartcase")
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.showmatch = true
+
+-- Wrapping & whitespace display
+vim.opt.wrap = false
+vim.opt.linebreak = true
+vim.opt.breakindent = true -- keep indent on wrapped lines
+vim.opt.breakindentopt = "list:-1" -- align list continuations under the text
+vim.opt.textwidth = 0 -- no auto line breaks; wrap is visual only
+vim.opt.list = true
+vim.opt.listchars = constants.listchars.normal
+
+-- UI
+vim.opt.number = true
+vim.opt.relativenumber = false
+vim.opt.cursorline = true
+vim.opt.scrolloff = 6
+vim.opt.cmdheight = 2
+vim.opt.laststatus = 3 -- one global statusline
+vim.opt.signcolumn = "yes" -- always on; prevents jitter
+vim.opt.showcmd = true
+vim.opt.showmode = false -- statusline already shows the mode
+vim.opt.wildmenu = true
+vim.opt.winborder = constants.floating_border
+vim.opt.shortmess = vim.opt.shortmess + "Ic" -- skip intro, silence completion chatter
+
+-- Buffer & editing behavior
+vim.opt.autoread = true
+vim.opt.hidden = true
+
+-- Splits
 vim.opt.splitbelow = true
 vim.opt.splitright = true
-vim.opt.tabstop = 2
-vim.opt.textwidth = 0
+
+-- Load project-local .nvimrc/.exrc, but only with `secure` restrictions.
+vim.opt.exrc = true
+vim.opt.secure = true
+
+-- Performance
+vim.opt.lazyredraw = true
+vim.opt.redrawtime = 10000 -- ms before giving up on syntax highlighting
+
+-- Timeouts: ttimeout* governs terminal escape sequences, timeoutlen governs mappings.
 vim.opt.ttimeout = true
 vim.opt.ttimeoutlen = 100
 vim.opt.timeoutlen = 500
-vim.opt.wildmenu = true
-vim.opt.winborder = constants.floating_border
-vim.opt.wrap = false
 
 vim.opt.fillchars:append({
   diff = "╱",
