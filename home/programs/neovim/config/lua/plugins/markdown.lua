c42b264a56a8636d98fe761a82382937209b9bbb
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2026-04-28 11:28:48 +0200

    Wrap in Markdown files in Neovim

--- home/programs/neovim/config/lua/plugins/markdown.lua
+++ home/programs/neovim/config/lua/plugins/markdown.lua
@@ -24,6 +24,7 @@ return {
         pattern = "markdown",
         callback = function()
           vim.opt_local.conceallevel = 2
+          vim.opt_local.wrap = true
         end,
       })
     end,
